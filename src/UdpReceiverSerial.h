27666dde80d37b221f06747369387d54f6c11292
Raoul Rubien <rubienr@gmail.com>
2020-03-21 20:48:31 +0100

    fixed include

--- src/UdpReceiverSerial.h
+++ src/UdpReceiverSerial.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <bridge/BridgeUdp.h>
+#include <BridgeUdp.h>
 #include <cstdint>
 
 // -------------------------------------------------------------------------------------------------
